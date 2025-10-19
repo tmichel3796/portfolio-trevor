933c50d8f4781a79481a75374150a5eafe03b4e3
tmichel3796 <tmichel3796@gmail.com>
2025-10-18 22:39:09 -0500

    app fix
    on running gunicorn could not find the app object, so it was having issues with starting application.

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import json,os
 
 #app = Flask(__name__)
 
-#app = Flask(__name__, static_folder='static', template_folder='templates')
+app = Flask(__name__, static_folder='static', template_folder='templates')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
